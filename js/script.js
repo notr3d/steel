a01738dceb3c49f9f7d5a59d0270121df86f8582
Александр Некрасов <lxmrrr@gmail.com>
2016-02-04 12:53:26 +0300

    contacts, about

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var myCenter=new google.maps.LatLng(55.788945, 37.72926);
+
+function initialize(){
+    var mapProp = {
+      center:myCenter,
+      zoom:16,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+
+    var map=new google.maps.Map(document.getElementById("map"), mapProp);
+
+    var marker=new google.maps.Marker({
+      position:myCenter,
+      icon:'img/map-icon.png',
+      size: new google.maps.Size(20, 32)
+      });
+
+    marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
