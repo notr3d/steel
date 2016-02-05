7f6bc5ded0ba07ed93b06e9c68d2ad026ea8d46b
Александр Некрасов <lxmrrr@gmail.com>
2016-02-05 15:04:57 +0300

    major improvments

--- js/script.js
+++ js/script.js
@@ -4,9 +4,14 @@ $(document).ready(function() {
         verticalCentered:false,
         fixedElements: '.page-nav',
         navigation:true,
-        slidesNavigation:true,
+        //slidesNavigation:true,
         navigationTooltips: ['firstSlide', 'production', 'thirdSlide'],
-        scrollingSpeed:500,
+        scrollingSpeed:400,
+        afterRender:function(){
+            slideTimeout = setInterval(function(){
+                $.fn.fullpage.moveSlideRight();
+            }, 5000);
+        },
     });
     /*$('.slider').slick({
       autoplay:true,
