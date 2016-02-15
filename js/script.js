142fff53feb69d7e13a7cca3bfcbd0d255149718
Александр Некрасов <lxmrrr@gmail.com>
2016-02-15 10:51:34 +0300

    additional fixes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+	
     $('.scrollup').click(function(){
         $('body').animate({scrollTop: 0}, 300);
         return false;
     });
+	
     $('#fullpage').fullpage({
         scrollBar:false,
         verticalCentered:false,
